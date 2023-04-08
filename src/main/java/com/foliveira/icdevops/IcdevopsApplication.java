14f0927cb3fd30439d820bee6c5b3ba68968e923
FelipOliveira <sem.ideias.na.hora@gmail.com>
2023-04-08 11:40:12 -0300

    first commit

---
+++ src/main/java/com/foliveira/icdevops/IcdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.foliveira.icdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcdevopsApplication.class, args);
+	}
+
+}
